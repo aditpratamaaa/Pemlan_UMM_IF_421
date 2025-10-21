6cdb1a5c307032e2a8d82292713a2e12b8d926f8
aditpratamaaa <aditspensa7c@gmail.com>
2025-10-21 10:03:27 +0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args ){
+        Scanner input = new Scanner(System.in);
+        System.out.println(">>>Data Mahasiswa<<<");
+
+        System.out.println("Masukkan Nama Mahasiswa: ");
+        String nama = input.nextLine();
+
+        System.out.println("Masukkan Nilai Mahasiswa: ");
+        int nilai = input.nextInt();
+
+        System.out.println("Data Mahasiswa: ");
+        System.out.println("Nama: " + nama);
+        System.out.println("Nilai: " + nilai);
+
+
+    }
+}
